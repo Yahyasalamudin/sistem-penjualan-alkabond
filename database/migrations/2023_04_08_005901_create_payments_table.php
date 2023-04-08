d6ed26cd0d7b161dcfef92be00c238d45ce24fb3
SriAdiC <21sacah002@gmail.com>
2023-04-08 08:23:12 +0700

    add db migration

---
+++ database/migrations/2023_04_08_005901_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('total_pay');
+            $table->foreignId('transaction_id')->constrained('transactions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
