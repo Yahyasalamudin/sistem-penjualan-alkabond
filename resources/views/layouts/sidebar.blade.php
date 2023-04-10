f82fbaab922cfe507c045101b552498e16f2b4e1
dwik13 <dwikhusnul632@gmail.com>
2023-04-10 22:21:15 +0700

    dashboard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,10 +2,11 @@
 
      <!-- Sidebar - Brand -->
      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-         <div class="sidebar-brand-icon rotate-n-15">
-             <i class="fas fa-laugh-wink"></i>
+         <div class="sidebar-brand-icon ">
+             {{-- <i class="fas fa-laugh-wink"></i> --}}
+             <img src="{{ asset("img/logo.png") }}" width="40px" alt="">
          </div>
-         <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+         <div class="sidebar-brand-text mx-3">Sejahtera Bersama</div>
      </a>
 
      <!-- Divider -->
